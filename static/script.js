aaefecb374da112aeb5ea158e93af62f0950b07b
Anthony Kummerfeldt <73972025+Kumm-feldt@users.noreply.github.com>
2023-12-23 22:23:33 -0500

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,12 @@
+function toggleEditField() {
+   window.location.href = "/edit";
+
+  }
+  function toggleAdd() {
+   window.location.href = "/add";
+
+  }
+function toggleAddOrder(){
+   window.location.href = "/receipt/user";
+  }
+
